9a38d7c92ff451d8720549856b40d1808f474748
Huberth <vladimirhuberth@hotmail.com>
2021-10-17 23:12:03 -0300

    test: server table teste finished

--- src/__test__/functions/serverTable.test.ts
+++ src/__test__/functions/serverTable.test.ts
@@ -1,18 +1,23 @@
 import { verifyArraySelectedServers } from "../../functions/serverTable";
 
 describe("server Table function", () => {
-  it("Should verify if value exist in server", () => {
-    const dados = {
-      hostname: "Pc",
-      configuracao: {
-        cpuProvisioned: 2,
-        memoryProvisioned: 2,
-        totalDiskGB: 15,
-      },
-      ip: "10.0.0.0",
-    };
+  const dados = {
+    hostname: "Pc",
+    configuracao: {
+      cpuProvisioned: 2,
+      memoryProvisioned: 2,
+      totalDiskGB: 15,
+    },
+    ip: "10.0.0.0",
+  };
 
+  it("Shoud return a empty array", () => {
     const array = verifyArraySelectedServers([dados], dados);
     expect(array).toEqual([]);
+  });
+
+  it("Should return one array", () => {
+    const array = verifyArraySelectedServers([], dados);
+    expect(array).toEqual([dados]);
   });
 });
